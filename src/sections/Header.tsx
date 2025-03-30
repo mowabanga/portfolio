7340eb2eb5b4bef1f790c4e980797c8c199217a5
mowabanga <itsmundia@gmail.com>
2025-03-30 16:55:15 +0300

    Add hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import { FC } from "react";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
@@ -26,7 +27,7 @@ const navItems = [
 
 const Header: FC = () => {
   return (
-    <header>
+    <header className="fixed top-0 left-0 w-full backdrop-blur-md">
       <div className="container !max-w-full ">
         <div className="flex justify-between h-20 items-center">
           <div>
@@ -35,13 +36,13 @@ const Header: FC = () => {
             </a>
           </div>
           <div className="flex items-center gap-4">
-            <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center">
+            <div className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="3" y="15" width="18" height="2" fill="currentColor" />
                 <rect x="3" y="7" width="18" height="2" fill="currentColor" />
               </svg>
             </div>
-            <button className="bg-red-orange-500 h-11 px-6 rounded-xl text-white border border-red-orange-500 uppercase hidden md:inline-flex items-center">Contact Me</button>
+            <Button variant="primary" className="hidden md:inline-flex">Contact Me</Button>
           </div>
         </div>
       </div>
